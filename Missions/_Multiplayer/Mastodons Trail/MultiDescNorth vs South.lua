0ceb60e7cd41c7515837afdd402e74d57ef932fe
TomKulik <tzar126el@gmail.com>
2020-12-02 01:45:11 +0100

    0.28v

---
+++ Missions/_Multiplayer/Mastodons Trail/MultiDescNorth vs South.lua
@@ -0,0 +1,19 @@
+TECHNOLOGYLIMITS = { [1] = true , [2] =  true },
+
+LOCKEDTEAMS = { [1] = false , [2] =  fasle },
+
+POSCOORS = {
+	[1] ={ X=  60, Y = 80, },
+	[2] ={ X= 165, Y = 90, },
+	[3] ={ X= 205, Y = 45, },
+	[4] ={ X= 425, Y = 35, },
+	[5] ={ X=  80, Y = 285, },
+	[6] ={ X= 295, Y = 275, },
+	[7] ={ X= 350, Y = 205, },
+	[8] ={ X= 460, Y = 225, },
+},
+
+RESTRICTIONS = {
+	LOCKED_T_OPT = {[1] = 25, },
+},
+
