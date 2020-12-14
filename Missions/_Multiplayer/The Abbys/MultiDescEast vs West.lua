59f47efe59d0cf2ebee1b432726b2da8c5eff4e6
TomKulik <tzar126el@gmail.com>
2020-12-14 23:55:07 +0100

    0.29v

--- Missions/_Multiplayer/The Abbys/MultiDescEast vs West.lua
+++ Missions/_Multiplayer/The Abbys/MultiDescEast vs West.lua
@@ -3,10 +3,10 @@ TECHNOLOGYLIMITS = { [1] = true , [2] =  true },
 LOCKEDTEAMS = { [1] = false , [2] =  true },
 
 POSCOORS = {
-	[1] ={ X= 95, Y = 95, },
-	[2] ={ X= 350, Y = 50, },
-	[3] ={ X= 125, Y = 260, },
-	[4] ={ X= 370, Y = 235, },
+	[1] ={ X= 70, Y = 100, },
+	[2] ={ X= 70, Y = 240, },
+	[3] ={ X= 436, Y = 100, },
+	[4] ={ X= 436, Y = 240, },
 },
 
 RESTRICTIONS = {
