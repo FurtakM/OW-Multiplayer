8659f22a3168f45a147761c5b9381cd4ce6b105e
FurtakM <serpentow@gmail.com>
2021-03-29 07:11:11 +0200

    0.35v

---
+++ Missions/_Multiplayer/Decravass/MultiDescOneOnOne.lua
@@ -0,0 +1,12 @@
+TECHNOLOGYLIMITS = { [1] = true , [2] =  true },
+
+LOCKEDTEAMS = { [1] = false , [2] =  false },
+
+POSCOORS = {
+	[1] ={ X = 89, Y = 85, },
+	[2] ={ X = 410, Y = 240, },
+},
+
+RESTRICTIONS = {
+},
+
---
+++ Missions/_Multiplayer/Mountain Labyrinth/MultiDescEast vs West.lua
@@ -0,0 +1,16 @@
+TECHNOLOGYLIMITS = { [1] = true , [2] =  true },
+
+LOCKEDTEAMS = { [1] = false , [2] =  false },
+
+POSCOORS = {
+	[1] = { X = 36, Y = 41, },
+	[2] = { X = 36, Y = 141, },
+	[3] = { X = 36, Y = 255, },
+	[4] = { X = 460, Y = 41, },
+	[5] = { X = 460, Y = 141, },
+	[6] = { X = 460, Y = 255, },
+},
+
+RESTRICTIONS = {
+},
+
