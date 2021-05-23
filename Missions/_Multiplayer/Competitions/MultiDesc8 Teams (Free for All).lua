0d668c9de05705efe1cc9a84b8375d75cc47b918
FurtakM <serpentow@gmail.com>
2021-05-23 04:02:51 +0200

    0.36v

---
+++ Missions/_Multiplayer/Competitions/MultiDesc8 Teams (Free for All).lua
@@ -0,0 +1,19 @@
+TECHNOLOGYLIMITS = { [1] = true , [2] =  true },
+
+LOCKEDTEAMS = { [1] = false , [2] =  false },
+
+POSCOORS = {
+	[1] ={ X= 55, Y = 96, },
+	[2] ={ X= 180, Y = 38, },
+	[3] ={ X= 331, Y = 38, },
+	[4] ={ X= 450, Y = 96, },
+	[5] ={ X= 450, Y = 212  },
+	[6] ={ X= 331, Y = 290, },
+	[7] ={ X= 170, Y = 290, },
+	[8] ={ X= 55, Y = 216, }
+},
+
+RESTRICTIONS = {
+	LOCKED_T_OPT = {[1] = 25, },
+},
+
